d1138e03df442213fa00e134817bb8b01fbf05dd
muahchee <187490843+muahchee@users.noreply.github.com>
2025-03-03 22:31:31 +1300

    make option menu open and close

--- src/scripts/task.js
+++ src/scripts/task.js
@@ -22,13 +22,32 @@ export class Task {
 
     this.optionBtn = document.createElement("button");
     this.optionBtn.setAttribute("class", "options");
+    //"optionInsideClick" is to allow option window to close when clicking outside, prevent inside clicks from closeing the window
+    this.optionBtn.setAttribute("optionInsideClick", true);
 
     this.optionIcon = document.createElement("img");
     this.optionIcon.src = optionImg;
-    this.optionIcon.setAttribute("alt", "menu icon")
+    this.optionIcon.setAttribute("alt", "menu icon");
+    this.optionIcon.setAttribute("optionInsideClick", true);
+
+    //options menu
+    this.optionMenu = document.createElement("div");
+    this.optionMenu.setAttribute("class", "options-menu");
+    this.optionMenu.setAttribute("optionInsideClick", true);
+
+    this.editBtn = document.createElement("button");
+    this.editBtn.setAttribute("class", "edit");
+    this.editBtn.textContent = "Edit";
+    this.editBtn.setAttribute("optionInsideClick", true);
+
+    this.deleteBtn = document.createElement("button");
+    this.deleteBtn.setAttribute("class", "delete");
+    this.deleteBtn.textContent = "Delete";
+    this.deleteBtn.setAttribute("optionInsideClick", true);
 
   }
 
+  //--private interfaces--
   _format(string){
     //split the taskContent string by spaces and join together with "-"
 
@@ -38,16 +57,38 @@ export class Task {
     .join("-");
   }
 
+  _outsideClick(element){
+    document.addEventListener("click", e => {
+      if (!element.contains(e.target)) {
+        element.style.visibility = "hidden"
+      }
+    })
+  }
 
+  //--public interfaces--
   createTask(){
     this.taskList.appendChild(this.task);
 
     this.task.appendChild(this.input);
     this.task.appendChild(this.label);
     this.task.appendChild(this.optionBtn);
+    this.task.appendChild(this.optionMenu)
 
     this.optionBtn.appendChild(this.optionIcon)
 
-  }
+    this.optionMenu.appendChild(this.editBtn);
+    this.optionMenu.appendChild(this.deleteBtn);
 
+    //open option menu
+    this.optionBtn.addEventListener("click", () => {
+      this.optionMenu.style.visibility = "visible";
+    });
+
+    //close option menu
+    document.addEventListener("click", e => {
+      if (!e.target.getAttribute("optionInsideClick")){
+        this.optionMenu.style.visibility = "hidden";
+      }
+    })
+  }
 }
